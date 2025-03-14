c296c0b9911c735beb35113658e51092b8b751e6
John <jsameh16@gmail.com>
2025-03-14 13:21:51 +0200

    add notifications

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/notification.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
@@ -7,10 +8,25 @@ class NotificationsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF4F245A),
-
-      body: Center(
-        child: Text('Notifications Screen'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            NotificationWidget(
+              name: 'John Doe',
+              date: '14/3/2025',
+              profilePic: 'assets/profile_picture.png',
+              text: 'Joined the Community, Say Hi!',
+            ),
+            NotificationWidget(
+              name: 'John Doe',
+              date: '14/3/2025',
+              profilePic: 'assets/profile_picture.png',
+              text: 'Added You as a Friend!',
+            ),
+          ],
+        ),
       ),
+
     );
   }
 }
---
+++ lib/widgets/notification.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class NotificationWidget extends StatelessWidget {
+  final String name;
+  final String profilePic;
+  final String text;
+  final String date;
+
+  const NotificationWidget({
+    super.key,
+    required this.name,
+    required this.profilePic,
+    required this.text,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF3D1B45), 
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(3),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFFFFE4A7), 
+                ),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(profilePic),
+                  radius: 20,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Center(
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                date,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
